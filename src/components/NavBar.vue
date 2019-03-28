23370f5b287956e0b26401aa1dc5fecf9b48c179
anahimce <ana_himce@yahoo.com>
2019-03-28 18:44:27 +0100

    first

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <router-link class="navbar-brand" to="/trivia">Trivia App</router-link>
+    <div class="navbar-nav w-100">
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  components: {
+  },
+  
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+import TriviaApp from './components/TriviaApp'
+
+
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+
+const routes = [
+  { path: '/', component: TriviaApp },
+  { path: '/trivia', component: TriviaApp },
+ 
+]
+const router = new VueRouter({
+  routes 
+})
+
+
 new Vue({
+  router,
+  mode: 'history',
   render: h => h(App),
 }).$mount('#app')
